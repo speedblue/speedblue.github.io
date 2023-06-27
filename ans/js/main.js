cce1dc96a9c33f4537e01165ac7676a59b455ea5
Julien Lemoine <speedblue@happycoders.org>
2023-06-27 08:43:21 +0200

    Some cosmetic changes

--- ans/js/main.js
+++ ans/js/main.js
@@ -2,9 +2,13 @@ var Airtable = require('airtable');
 
 var base = null
 var textSize = 5;
+var maxHours = 120;
 var events = [];
 var firstLoad = true;
 
+function deltaToHours(timeInSec) {
+    return timeInSec / 3600
+}
 function displayTime(timeInSec) {
     const sec = (Math.floor(timeInSec) % 60)
     const rest = Math.floor(timeInSec / 60)
@@ -22,6 +26,12 @@ function arraySortFunction(a, b)
     return 0;
 }
 
+function sameDay(day1, day2) {
+    return day1.getFullYear() == day2.getFullYear() && day1.getDate() == day2.getDate()
+}
+function getHourFormat(date) {
+    return (date.getHours() < 10 ? '0' : '') + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
+}
 function displayEvents() {
     console.log("DISPLAY :" + events.length)
 
@@ -30,8 +40,16 @@ function displayEvents() {
     var toDisplay = []
     for (var i = 0; i < events.length; ++i) {
         const date = new Date(events[i].date)
-        if (date > now) {
-            toDisplay.push({ "deltaInS": Math.floor((date - now) / 1000), "content": events[i].content, "time": date.toLocaleString()})
+        if (date > now) { // remove past events
+            var delta = Math.floor((date - now) / 1000)
+            if (deltaToHours(delta) < maxHours) { // remove events too much in the future
+                var dateString = ""
+                if (sameDay(date, now))
+                    dateString = getHourFormat(date)
+                else
+                    dateString = date.getDate() + '/' + (date.getMonth() + 1) + ' ' + getHourFormat(date);
+                toDisplay.push({ "deltaInS": delta, "content": events[i].content, "time": dateString})
+            }
         }
     }
     // Sort array and keep maximum 5 elements to display
@@ -40,7 +58,7 @@ function displayEvents() {
         toDisplay.length = 5
     }
     var content = "";
-    content = '<table class="table table-striped" style="font-size: ' + textSize + 'vw;"><thead><tr><th scope="col">Time</th><th scope="col">Remaining</th><th>Event</th></tr></thead><tbody>';
+    content = '<table class="table table-striped" style="font-size: ' + textSize + 'vw; text-align: center;"><thead><tr><th scope="col" style="padding-top: 0;">Time</th><th scope="col" style="padding-top: 0;">Countdown</th><th style="padding-top: 0;">Event</th></tr></thead><tbody>';
     for (var i = 0; i < toDisplay.length; ++i) {
         content += '<tr><td>' + toDisplay[i].time + '</td><td>' + displayTime(toDisplay[i].deltaInS) + "</td>";
         content += '<td>' + toDisplay[i].content + '</td></tr>';
@@ -76,50 +94,58 @@ function reloadData() {
     firstLoad = false;
   });
 }
+function setLabelActive(name, element) {
+    for (var i = 1; i < 6; ++i) {
+        var component = i + name;
+        if (i == element)
+            document.getElementById(component).className = "dropdown-item active";
+        else
+            document.getElementById(component).className = "dropdown-item";
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-    base = new Airtable({ apiKey: 'patHSL6ZkPWx8Rkva.f0b8c1970c1cd8b5926d04eaf59d9fd500a39738c73bbb3a471f4f7eb3561ec0' }).base('appiiuioD2YBj4Da6');
-
+   base = new Airtable({ apiKey: 'patHSL6ZkPWx8Rkva.f0b8c1970c1cd8b5926d04eaf59d9fd500a39738c73bbb3a471f4f7eb3561ec0' }).base('appiiuioD2YBj4Da6');
    reloadData();
    document.getElementById('1TConfig').onclick = function() {
-        document.getElementById('1TConfig').className = "dropdown-item active";
-        document.getElementById('2TConfig').className = "dropdown-item ";
-        document.getElementById('3TConfig').className = "dropdown-item ";
-        document.getElementById('4TConfig').className = "dropdown-item";
-        document.getElementById('5TConfig').className = "dropdown-item";
+       setLabelActive('TConfig', 1);
        textSize = 1;
    }
     document.getElementById('2TConfig').onclick = function() {
-        document.getElementById('1TConfig').className = "dropdown-item ";
-        document.getElementById('2TConfig').className = "dropdown-item active";
-        document.getElementById('3TConfig').className = "dropdown-item ";
-        document.getElementById('4TConfig').className = "dropdown-item";
-        document.getElementById('5TConfig').className = "dropdown-item";
+        setLabelActive('TConfig', 2);
         textSize = 2;
     }
     document.getElementById('3TConfig').onclick = function() {
-        document.getElementById('1TConfig').className = "dropdown-item ";
-        document.getElementById('2TConfig').className = "dropdown-item ";
-        document.getElementById('3TConfig').className = "dropdown-item active";
-        document.getElementById('4TConfig').className = "dropdown-item";
-        document.getElementById('5TConfig').className = "dropdown-item";
+        setLabelActive('TConfig', 3);
         textSize = 3;
     }
     document.getElementById('4TConfig').onclick = function() {
-        document.getElementById('1TConfig').className = "dropdown-item ";
-        document.getElementById('2TConfig').className = "dropdown-item ";
-        document.getElementById('3TConfig').className = "dropdown-item ";
-        document.getElementById('4TConfig').className = "dropdown-item active";
-        document.getElementById('5TConfig').className = "dropdown-item";
+        setLabelActive('TConfig', 4);
         textSize = 4;
     }
     document.getElementById('5TConfig').onclick = function() {
-        document.getElementById('1TConfig').className = "dropdown-item ";
-        document.getElementById('2TConfig').className = "dropdown-item ";
-        document.getElementById('3TConfig').className = "dropdown-item ";
-        document.getElementById('4TConfig').className = "dropdown-item";
-        document.getElementById('5TConfig').className = "dropdown-item active";
+        setLabelActive('TConfig', 5);
         textSize = 5;
     }
+    document.getElementById('1DConfig').onclick = function() {
+        setLabelActive('DConfig', 1);
+        maxHours = 24;
+    }
+     document.getElementById('2DConfig').onclick = function() {
+         setLabelActive('DConfig', 2);
+         maxHours = 48;
+     }
+     document.getElementById('3DConfig').onclick = function() {
+         setLabelActive('DConfig', 3);
+         maxHours = 72;
+     }
+     document.getElementById('4DConfig').onclick = function() {
+         setLabelActive('DConfig', 4);
+         maxHours = 96;
+     }
+     document.getElementById('5DConfig').onclick = function() {
+         setLabelActive('DConfig', 5);
+         maxHours = 120;
+     }
 });
 
